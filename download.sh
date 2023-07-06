3be8739ce5cd798e06c8278a49ce5b4aa97a346c
Festere <104324449+festere@users.noreply.github.com>
2023-07-06 15:01:15 +0200

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,38 @@
+# Update the system:
+apt update && upgrade -y
+apt-get update 
+
+# Download the apps:
+apt-get install software-properties-common -y
+apt install apache2 -y
+apt install apache2-dev -y
+apt install git -y
+apt-get install rabbitmq-server -y
+apt install docker.io -y
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
+apt install python3-pip -y
+apt-get install libapache2-mod-wsgi-py3 -y
+
+# Modify Apach2
+source_file="000-default.conf"
+destination_folder="/etc/apache2/sites-available"
+cp "$source_file" "$destination_folder/000-default.conf"
+systemctl restart apache2
+
+# Start the apps:
+systemctl enable rabbitmq-server
+service rabbitmq-server start
+rm -rf /var/snap/docker/179/run/docker.pid
+systemctl start docker
+
+# Download the plateforme:
+cd /var/www/
+git clone https://github.com/festere/plav.git
+cd plav/
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+# Install wsgi:
+a2enmod wsgi
