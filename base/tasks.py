ae313dc9cf770022e13bcb9650bb4952544b354c
Festere <104324449+festere@users.noreply.github.com>
2023-07-18 14:26:56 +0200

    Update tasks.py

--- base/tasks.py
+++ base/tasks.py
@@ -248,5 +248,5 @@ def upload_file(request):
 
     else:
         # Render the file upload form
-        print(Fore.RED + "Unseccessful analysis at: " + Fore.RESET + file_path + Fore.GREEN + " with file name: " + Fore.RESET + uploaded_file.name)
+        print(Fore.RED + "Unseccessful analysis at" + Fore.RESET)
         return render(request, 'home.html')
